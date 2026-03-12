bce7e46e32f6e46c1dca1573005b934d6abe3589
SohanaSadique <parna427@gmail.com>
2026-03-12 16:13:21 -0600

    Adding readme file

---
+++ README.R
@@ -0,0 +1,5 @@
+
+Assignment: Supervised Learning
+
+This assignment focuses on developing your understanding and application of Random Forest algorithms. You will work with the Can Path Student Dataset to perform a Random Forest analysis,
+conduct detailed hyperparameter tuning, and compare the performance of your model with other models or configurations. This exercise emphasizes building robust models and evaluating their performance critically.
